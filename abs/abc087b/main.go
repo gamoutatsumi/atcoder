6e5c5ca61d0dfc022742ab59aa28f8164c0e6302
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-05-15 13:12:29 +0900

    update

--- abs/abc087b/main.go
+++ abs/abc087b/main.go
@@ -25,5 +25,19 @@ func StrStdin() (stringInput [][]string) {
 
 func main() {
   p := StrStdin()
-  fmt.Println(p)
+  count := 0
+  a, _ := strconv.Atoi(p[0][0])
+  b, _ := strconv.Atoi(p[1][0])
+  c, _ := strconv.Atoi(p[2][0])
+  x, _ := strconv.Atoi(p[3][0])
+  for i := 0; i < a+1; i++ {
+    for j := 0; j < b+1; j++ {
+      for k := 0; k < c+1; k++ {
+        if ((i * 500 + j * 100 + k * 50) == x) {
+          count++
+        }
+      }
+    }
+  }
+  fmt.Println(count)
 }
